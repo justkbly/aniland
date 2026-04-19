775dcfee63fe5fd941c0bf3bfba6606044c5d864
kubi <kblyben@gmail.com>
2026-04-19 12:28:29 +0300

    Increase data size limit from 10MB to 50MB

--- server.js
+++ server.js
@@ -457,7 +457,7 @@ function readBody(req) {
     let data = '';
     req.on('data', chunk => {
       data += chunk;
-      if (data.length > 10e6) reject(new Error('Too large'));
+      if (data.length > 50e6) reject(new Error('Too large'));
     });
     req.on('end', () => {
       if (!data.trim()) { resolve({}); return; }
